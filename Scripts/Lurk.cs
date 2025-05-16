a6d91de1bdb6dc84efb5726bfd483cd163c15c4a
Morgan A. <morgan.g.abrams@gmail.com>
2025-05-15 22:24:44 -0400

    Create Lurk.cs

---
+++ Scripts/Lurk.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace CustomNamespace
+{
+    public class CustomClass
+    {
+        public object Run()
+        {
+            // The Main Variable
+            string arguments = "$allargs";
+            if (arguments == "")
+                return " ";
+
+            // Check Food
+            string[] foodArray = {"food", "breakfast", "lunch", "dinner", "cook", "snack", "brunch"};
+            if (SearchArray(foodArray, arguments))
+                return "food";
+
+            // Check Games
+            string[] gameArray = {"games", "play"};
+            if (SearchArray(gameArray, arguments))
+                return "game";
+
+            // Return empty string if not special
+            return " ";
+        }
+        
+        public bool SearchArray(string[] array, string arg)
+        {
+            foreach(var item in array)
+            {
+                if(arg.Contains(item))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
