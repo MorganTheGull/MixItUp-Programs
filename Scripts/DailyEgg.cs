a76c50fc2504a960b7acbafbc9509301e18a7fc6
Morgan A. <morgan.g.abrams@gmail.com>
2025-09-09 23:33:33 -0400

    Why was I using an if. This is cleaner.

--- Scripts/DailyEgg.cs
+++ Scripts/DailyEgg.cs
@@ -16,28 +16,20 @@ namespace CustomNamespace
 
             // Cooking the Egg
             Random rnd = new Random();
-            int cookStyle = rnd.Next(1, 9); // (1,9) is a random int between 1 and 8.
-            string cook = "This egg shall be eaten raw. Gross.";
-            // Note: This was a switch statement, then I transferred it into the script section on the MixItUp Bot.
-            // And it didn't work til I swapped it to an if-else. It always failed with the switch. >:/
-            if (cookStyle == 1)
-                cook = "Perhaps we boil this egg. But how do we want the yolk?";
-            else if (cookStyle == 2)
-                cook = "I'm thinking we poach this egg. A nice and runny yolk.";
-            else if (cookStyle == 3)
-                cook = "I'm feeling happy, so lets have the egg match. A nice sunny-side up egg.";
-            else if (cookStyle == 4)
-                cook = "Lets flip that egg a lil, and make it over easy.";
-            else if (cookStyle == 5)
-                cook = "Life is a lil chaotic. Shall we show that with our egg? A chaotic scrambled egg. YUM!";
-            else if (cookStyle == 6)
-                cook = "One baked egg coming up!";
-            else if (cookStyle == 7)
-                cook = "Is it getting steamy in here for anyone else? Cause this is becoming a steamed egg.";
-            else if (cookStyle == 8)
-                cook = "Omelet anyone? No, well to bad! One cheese and ham omelet coming up.";
+            int eggChoice = rnd.Next(1, 9); // (1,9) is a random int between 1 and 8.
+			string[] eggCook = 
+			{
+				"Perhaps we boil this egg. But how do we want the yolk?",
+				"I'm thinking we poach this egg. A nice and runny yolk.",
+				"I'm feeling happy, so lets have the egg match. A nice sunny-side up egg.",
+				"Lets flip that egg a lil, and make it over easy.",
+				"Life is a lil chaotic. Shall we show that with our egg? A chaotic scrambled egg. YUM!",
+				"One baked egg coming up!",
+				"Is it getting steamy in here for anyone else? Cause this is becoming a steamed egg.",
+				"Omelet anyone? No, well to bad! One cheese and ham omelet coming up.",
+			};
             
-            return chatMessage + cook;
+            return chatMessage + eggCook[eggChoice];
         }
     }
 }
